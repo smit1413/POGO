a68a92c0ef9d29c058f1aa484659e67911dfb74c
Richard <RICHARD@E7450-SSD>
2019-03-29 01:57:39 -0400

    Pokedex - Remove (a lot of) unused code

--- ProjectMeowth/Pokedex.aspx.cs
+++ ProjectMeowth/Pokedex.aspx.cs
@@ -18,8 +18,6 @@ namespace ProjectMeowth
             if(!IsPostBack)
             {
 
-                //Response.Cookies["myCookie"].Value = "some cookie value";
-                //var cookieValue = Request.Cookies["myCookie"].Value;
             }
 
             PokedexFakeGenerator generator = new PokedexFakeGenerator();
@@ -31,8 +29,8 @@ namespace ProjectMeowth
             PokedexGrid.Controls.Add(bootStrapRow);
 
 
-            //lstPokemon.ForEach(x =>
-            lstPokemon.Take(20).ToList<PokemonModel>().ForEach(x =>
+            lstPokemon.ForEach(x =>
+            //lstPokemon.Take(20).ToList<PokemonModel>().ForEach(x =>
             {
                 string customClass = "Generation-" + x.Generation.ToString() + " " + x.PokemonType;
 
@@ -48,27 +46,6 @@ namespace ProjectMeowth
 
                 HtmlGenericControl cardBody = new HtmlGenericControl();
                 cardBody.Attributes.Add("class", "card-body");
-
-                //////ImageButton cardImage = new ImageButton();
-                //////cardImage.Attributes.Add("class", "card-img-top mx-auto");
-                //////cardImage.Attributes.Add("data-speciesid", "SpeciesID-" + x.SpeciesID);
-                //////cardImage.Click += TogglePokemonState;
-
-                ////////HtmlImage innerImg = new HtmlImage();
-                //////String imageURL = "/Content/img/pokemon/" + x.SpeciesID + ".png";
-                //////if (File.Exists(Server.MapPath(imageURL)))
-                //////{
-                //////    //innerImg.Src = imageURL;
-                //////    cardImage.ImageUrl = imageURL;
-                //////    cardImage.Attributes.Add("style", "max-width: 150px");
-                //////    //innerImg.Attributes.Add("style", "max-width: 150px");
-                //////}
-                //cardImage.Controls.Add(innerImg);
-
-                ///
-                ////Label lblImg = new Label();
-                ////lblImg.Text = "\u2713";
-                ////lblImg.Visible = false;
 
                 HtmlGenericControl cardImage = new HtmlGenericControl();
                 cardImage.Attributes.Add("class", "card-img-top mx-auto");
@@ -88,77 +65,14 @@ namespace ProjectMeowth
                 cardTitle.Attributes.Add("class", "card-title");
                 cardTitle.InnerText = x.Name;
 
-                ////HtmlAnchor cardLink = new HtmlAnchor();
-                ////cardLink.HRef = "#";
-                ////cardLink.Attributes.Add("class", "btn btn-sm btn-info");
-                ////cardLink.InnerText = "Need this";
-                Label lblImg = new Label();
-                lblImg.Text = "\u2713";
-                lblImg.Visible = false;
-
                 cardBody.Controls.Add(cardTitle);
-                //cardBody.Controls.Add(cardLink);
-                cardBody.Controls.Add(lblImg);
                 cardContents.Controls.Add(cardImage);
                 cardContents.Controls.Add(cardBody);
 
                 
                 cardWrapper.Controls.Add(cardContents);
                 bootStrapRow.Controls.Add(cardWrapper);
-                //PokedexGrid.Controls.Add(cardWrapper);
-                //////////////////
-                
-                ////HtmlGenericControl panelOutside = new HtmlGenericControl();
-                ////panelOutside.Attributes.Add("class", "panel panel-success" + " grid-item " + customClass);
-                ////panelOutside.Attributes.Add("style", "min-height: 120px;");
-
-                ////HtmlGenericControl panelHeading = new HtmlGenericControl();
-                ////panelHeading.Attributes.Add("class", "panel-heading");
-
-                ////HtmlGenericControl panelBody = new HtmlGenericControl();
-                ////panelBody.Attributes.Add("class", "panel-body");
-
-                
-                ////ImageButton img = new ImageButton();
-                ////img.ImageUrl = "/Content/img/pokemon/" + x.SpeciesID + ".png";
-                ////img.Attributes.Add("data-speciesid", "SpeciesID-" + x.SpeciesID);
-                ////img.Click += TogglePokemonState;
-
-                ////Label lblImg = new Label();
-                ////lblImg.Text = "\u2713";
-                ////lblImg.Visible = false;
-
-                //////HtmlImage img = new HtmlImage();
-                //////img.Src = "/Content/img/pokemon/" + x.SpeciesID + ".png";
-
-                ////panelHeading.InnerText = x.Name;
-
-                ////panelBody.Controls.Add(img);
-                ////panelBody.Controls.Add(lblImg);
-
-                ////panelOutside.Controls.Add(panelHeading);
-                ////panelOutside.Controls.Add(panelBody);
-                ////PokedexGrid.Controls.Add(panelOutside);
-                
             });
-        }
-
-        public void TogglePokemonState(object sender, ImageClickEventArgs e)
-        {
-            ImageButton img = (ImageButton)sender;
-            String SpeciesID = img.Attributes["data-speciesid"];
-
-            
-            if (img.Style["opacity"] == "0.3")
-            {
-                img.Style.Remove("opacity");
-                img.Parent.Controls[1].Controls[1].Visible = false;
-            }
-            else
-            {
-                img.Style.Add("opacity", "0.3");
-                img.Parent.Controls[1].Controls[1].Visible = true;
-            }
         }
     }
 }
