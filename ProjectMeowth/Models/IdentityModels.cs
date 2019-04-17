a008796b166cb28158c3237eeb21a13d101dcd83
Richard <RICHARD@E7450-SSD>
2019-04-17 12:28:42 -0400

    UserProfile - Bind new attributes (PlayerName, Team, Experience)

--- ProjectMeowth/Models/IdentityModels.cs
+++ ProjectMeowth/Models/IdentityModels.cs
@@ -10,9 +10,14 @@ using ProjectMeowth.Models;
 
 namespace ProjectMeowth.Models
 {
+    // Customize & Expand the User Identity Profile
     // You can add User data for the user by adding more properties to your User class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public String PlayerName { get; set; }
+        public String Team { get; set; }
+        public int GameExperience { get; set; }
+
         public ClaimsIdentity GenerateUserIdentity(ApplicationUserManager manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
